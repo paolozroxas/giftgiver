8e8619e37b60de3d18265a754ba01cdd17e4721e
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-05-29 01:29:16 -0700

    adds gifts

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { enzyme } from 'enzyme';
+import { shallow } from 'enzyme';
+import App from './App.jsx';
+
+const app = shallow(<App />);
+
+describe('App', () => {
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('initializes `state` with empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
+
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
+
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([ { id: 1 } ]);
+    });
+
+    it('adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+
+    it('creates a Gift component', () => {
+      expect(app.find('Gift').exists()).toBe(true);
+    })
+
+  });
+
+
+})
---
+++ src/Gift.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
+class Gift extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: '',
+      present: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Form>
+            <FormGroup>
+              <ControlLabel>Person</ControlLabel>
+              <FormControl
+                className='input-person'
+                onChange={event => this.setState({ person: event.target.value })}
+              />
+
+              <ControlLabel>Present</ControlLabel>
+              <FormControl
+                className='input-present'
+                onChange={event => this.setState({ present: event.target.value })}
+              />
+            </FormGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Gift;
